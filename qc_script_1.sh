97a0213951ed26b0b7485fcac033f9219060bb63
Sam Yaple <sam@yaple.net>
2014-08-11 11:52:41 -0500

    Adjusted lines to fit with the 80 col convention; something are broke in script, I need a test enviroment for it. parameters are in place.

--- qc_script_1.sh
+++ qc_script_1.sh
@@ -1,22 +1,70 @@
 #!/bin/bash
 
 # USAGE:
-# ./script.sh [MGMT Subnet address]
+# ./qc.sh [options] [MGMT Subnet address]
 
-# TODO: Sanitize script so that it can be run repeatedly without anything breaking.
+# TODO: Sanitize script so that it can be run repeatedly
+
+###Default Params, overriden by cli options passed
+
+networking="true"
+resize_swap="false"
+system_upgrade="true"
+kernel_upgrade="false"
+dell_om="true"
+sol="true"
+restart_node="false"
+MGMT_SUBNET="10.240.0.0/22"
+
+OMCONFIG_BIN="/opt/dell/srvadmin/bin/omconfig"
+RACADM_BIN="/opt/dell/srvadmin/bin/omconfig"
 
 args=($@)
 
 usage () {
-	echo Full usage in coming days
+	cat << EOF
+Usage: qc [ OPTIONS ] [ MGMT_IP ]
+where OPTIONS := {
+			--without-networking, -n
+			--resize-swap, -r
+			--without-system-upgrade, -u
+			--kernel-upgrade, -k
+			--without-dell-om, -d
+			--without-serail-setup, -s
+			--no-restart
+		}
+
+examples:
+	Full Install
+		./qc -r -k
+
+	Full Install with uncommon MGMT
+		./qc -r -k 10.12.0.0/21
+
+	Partial Install with garunteed no restart
+		./qc --no-restart
+EOF
 }
 
 parameters () {
-	# TODO - Validate ip address in case statement 
+	# TODO - Validate ip address better. Maybe. Its not really a priority
+	# since if we don't find a valid interface we will search for another
+	# one anyway. Also, only the first two octets matter currently
+	
 	# TODO - Other arguments needed?
+
 	# Parse shell script parameters
 	# We use "false" instead of a boolean for readability
+	# We don't use getop/s because I have always hated them
+	# and I see no need in this scenario
+
 	for arg in "${args[@]}"; do
+
+	if [[ "${arg}" =~ ^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.*$ ]]; \
+									   then
+		MGMT_SUBNET="${arg}"
+		continue
+	fi
 
 	case "${arg}" in
 	"") 
@@ -26,14 +74,14 @@ parameters () {
 	--without-networking|-n)
 		networking="false"
 		;;
-	--without-resize-swap|-r)
-		resize_swap="false"
+	--resize-swap|-r)
+		resize_swap="true"
 		;;
 	--without-system-upgrade|-u)
 		system_upgrade="false"
 		;;
-	--without-kernel-upgrade|-k)
-		kernel_upgrade="false"
+	--kernel-upgrade|-k)
+		kernel_upgrade="true"
 		;;
 	--without-dell-om|-d)
 		dell_om="false"
@@ -44,8 +92,9 @@ parameters () {
 	--no-restart)
 		restart_node="false"
 		;;
-	[0-9].*)
-		MGMT_SUBNET="${arg}"
+	--help|-h)
+		usage
+		exit 0
 		;;
 	*)
 		echo "Unknown argument ${arg}"
@@ -57,36 +106,46 @@ parameters () {
 }
 
 bdf_sort () {
-	# This sorts by using some sed magic. It view groups as lines with breaks in between them. It uses "vertical tabs" to allow sorting. This only sorts based on the first line in each group. See Sam Yaple if it needs to sort by a line other than the first one.
-	# This does require Ubuntu to generate comment lines with "PCI device $attr{vendor}:$attr{device} ($driver)". I think 14.04 changes this. Works great on 12.04
-        # Sort by PCI BDF notation
-        sed -r ':r;/(^|\n)$/!{$!{N;br}};s/\n/\v/g' "${BAK_UDEV_NET}"    | \
-        sort                                                            | \
-        sed 's/\v/\n/g'                                                 | \
-        awk '/SUBSYSTEM/ {print $0}'                                    | \
-        awk 'BEGIN {i=0} {sub(/eth[0-9].*$/,"eth"i"\"") ; i++} {print $0}' > "${UDEV_NET}"
+	# This sorts by using some sed magic. It view groups as lines with
+	# breaks in between them. It uses "vertical tabs" to allow sorting.
+	# This only sorts based on the first line in each group. See Sam Yaple
+	# if it needs to sort by a line other than the first one.
+
+	# This does require Ubuntu to generate comment lines with "PCI device 
+	# $attr{vendor}:$attr{device} ($driver)". I think 14.04 changes this.
+	# Works great on 12.04
+
+	# Sort by PCI BDF notation
+	sed -r ':r;/(^|\n)$/!{$!{N;br}};s/\n/\v/g' "${BAK_UDEV_NET}"	    | \
+	sort								    | \
+	sed 's/\v/\n/g'							    | \
+	awk '/SUBSYSTEM/ {print $0}'					    | \
+	awk 'BEGIN {i=0} {sub(/eth[0-9].*$/,"eth"i"\"") ; i++} {print $0}' \
+								> "${UDEV_NET}"
 }
 
 bdf_mac_sort () {
-        # Sort by PCI BDF notation, use Bus Device then MAC address (instead of Function) for sorting
-        for MAC_VEN in $(sed -r ':r;/(^|\n)$/!{$!{N;br}};s/\n/\v/g' "${BAK_UDEV_NET}"   | \
-                                sort                                                    | \
-                                sed 's/\v/\n/g'                                         | \
-                                awk '/SUBSYSTEM/ {print $0}'                            | \
-                                awk -F\" '{print $8}'                                   | \
-                                awk -F: '{print $1":"$2":"$3}'                          | \
-                                uniq
-                        ); do
-                grep "${MAC_VEN}" "${BAK_UDEV_NET}" | sort;
-                        done | \
-                        awk 'BEGIN {i=0} {sub(/eth[0-9].*$/,"eth"i"\"") ; i++} {print $0}' > "${UDEV_NET}"
+	# Sort by PCI BDF notation, use Bus Device then MAC address for sorting
+	for MAC_VEN in $(
+	       sed -r ':r;/(^|\n)$/!{$!{N;br}};s/\n/\v/g' "${BAK_UDEV_NET}" | \
+	       sort  							    | \
+	       sed 's/\v/\n/g'						    | \
+	       awk -F\" '/SUBSYSTEM/ {print $8}'			    | \
+	       awk -F: '{print $1":"$2":"$3}'				    | \
+	       uniq
+			); do
+	       			grep "${MAC_VEN}" "${BAK_UDEV_NET}" | sort;
+	       done | \
+	 awk 'BEGIN {i=0} {sub(/eth[0-9].*$/,"eth"i"\"") ; i++} {print $0}' \
+								> "${UDEV_NET}"
 }
 
 udev_fix () {
 	UDEV_NET=/etc/udev/rules.d/70-persistent-net.rules
 	BAK_UDEV_NET=/root/70-persistent-net.rules.bak
 
-	# Remove existing rule and regenerate 70-persistent-net.rules with PCI names in comments
+	# Remove existing rule and regenerate 70-persistent-net.rules with PCI
+	# names in comments
 	rm "${UDEV_NET}"
 	udevadm trigger --action=add
 	udevadm settle
@@ -96,14 +155,17 @@ udev_fix () {
 
 	cp "${UDEV_NET}" "${BAK_UDEV_NET}"
 
-	# Using BDF MAC sorting as I believe that is the correct way. Still need to verify
+	# TODO: Using BDF MAC sorting as I believe that is the correct way.
+	# Still need to verify
 	bdf_mac_sort
 
-	# You need to reboot now, but wait until you are sure rearranging the names isn't going to hose your access when you reboot.
+	# You need to reboot now, but wait until you are sure rearranging the
+	# names isn't going to hose your access when you reboot.
 }
 
 interfaces_fix () {
 	# TODO: Generate /etc/network/interfaces
+	echo I did nothing
 }
 
 hosts_fix () {
@@ -113,11 +175,14 @@ hosts_fix () {
 	HOST="$(echo ${RS_SERVER_NAME} | awk -F. '{print $1}')"
 	FQDN="${RS_SERVER_NAME}"
 	MGMT_SUBNET="$(echo ${1:-10.240.0.0} | awk -F. {print $1"."$2})"
-	ADDRESS=$(ip a | awk '/inet "${MGMT_SUBNET}"/ {sub(/\/[0-9]+$/, "", $2); print $2; exit}')
+	ADDRESS=$(ip a | awk '/inet "${MGMT_SUBNET}"/ \
+				{sub(/\/[0-9]+$/, "", $2); print $2; exit}')
 
 	if [[ -z "${ADDRESS}" ]]; then
-		ADDRESS=$(ip a | awk '/inet/ {sub(/\/[0-9]+$/, "", $2); print $2; exit}')
-		echo "Failed to get management ip address. Assuming ${ADDRESS} is the management ip"
+		ADDRESS=$(ip a | awk '/inet/ {sub(/\/[0-9]+$/, "", $2); \
+							print $2; exit}')
+		echo "Failed to get management ip address. Assuming \
+					${ADDRESS} is the management ip"
 	fi
 	
 	cp "${HOSTS}" "${HOSTS_BAK}"
@@ -144,7 +209,9 @@ networking () {
 }
 
 volumes () { 
-	# Resize any swap disks larger than 8GB to 8GB - TODO Run only if 8G > swap00 > 7G, possibly check boot size as well
+	# Resize any swap disks larger than 8GB to 8GB - TODO Run only if \
+	# 8G => swap00 > 7G, possibly check boot size as well
+
 	if [[ "${resize_swap}" == "false" ]]; then
 		return 0
 	fi
@@ -158,7 +225,10 @@ volumes () {
 }
 
 modules () {
-	# Un-blacklist Modules - Line will only modify file if file exists and blacklist e1000e or blacklist ixgbe exists at the beginning of a line.
+	# Un-blacklist Modules - Line will only modify file if file exists \
+	# and blacklist e1000e or blacklist ixgbe exists at the beginning \ 
+	# of a line.
+
 	if [[ "${modules}" == "false" ]]; then
 		return 0
 	fi
@@ -171,7 +241,9 @@ modules () {
 	 
 	# TODO (Thomas M. / Charles F.): Need to upgrade ixgbe version here
 	# Add bonding and NIC modules to /etc/modules file
-	# We only need to add these lines if they don't exist. No need to add them repeatedly if the script is rerun
+	# We only need to add these lines if they don't exist. No need to add \
+	# them repeatedly if the script is rerun
+
 	MODULES="/etc/modules"
 	grep -q '^bonding$' "${MODULES}" || echo 'bonding' >> "${MODULES}"
 	grep -q '^e1000e$' "${MODULES}" || echo 'e1000e' >> "${MODULES}"
@@ -182,10 +254,13 @@ modules () {
 
 sol () { 
 	# Create file to configure console serial redirection over DRAC
-	# (this will allow you to access DRAC from your terminal session window)
+	# (this will allow access to DRAC from your terminal session window)
 	if [[ "${sol}" == "false" ]]; then
 		return 0
 	fi
+
+	OMCONFIG_BIN="/opt/dell/srvadmin/bin/omconfig"
+	RACADM_BIN="/opt/dell/srvadmin/bin/omconfig"
 	
 	cat << EOF > /etc/init/ttyS0.conf
 
@@ -208,27 +283,29 @@ EOF
 	start ttyS0
 
 	# Apply BIOS changes to allow console serial redirection over DRAC
-	/opt/dell/srvadmin/bin/omconfig chassis biossetup attribute=extserial setting=rad
-	/opt/dell/srvadmin/bin/omconfig chassis biossetup attribute=fbr setting=115200
-	/opt/dell/srvadmin/bin/omconfig chassis biossetup attribute=serialcom setting=com2
-	/opt/dell/srvadmin/bin/omconfig chassis biossetup attribute=crab setting=enabled
+	"${OMCONFIG_BIN}" chassis biossetup attribute=extserial setting=rad
+	"${OMCONFIG_BIN}" chassis biossetup attribute=fbr setting=115200
+	"${OMCONFIG_BIN}" chassis biossetup attribute=serialcom setting=com2
+	"${OMCONFIG_BIN}" chassis biossetup attribute=crab setting=enabled
  
 	# Apply DRAC settings to allow console serial redirection over DRAC
-	/opt/dell/srvadmin/sbin/racadm config -g cfgSerial -o cfgSerialBaudRate 115200
-	/opt/dell/srvadmin/sbin/racadm config -g cfgSerial -o cfgSerialConsoleEnable 1
-	/opt/dell/srvadmin/sbin/racadm config -g cfgSerial -o cfgSerialSshEnable 1
-	/opt/dell/srvadmin/sbin/racadm config -g cfgSerial -o cfgSerialHistorySize 2000
+	"${RACADM_BIN}" config -g cfgSerial -o cfgSerialBaudRate 115200
+	"${RACADM_BIN}" config -g cfgSerial -o cfgSerialConsoleEnable 1
+	"${RACADM_BIN}" config -g cfgSerial -o cfgSerialSshEnable 1
+	"${RACADM_BIN}" config -g cfgSerial -o cfgSerialHistorySize 2000
 }
  
 update_kernel () {
-	# TODO: Wrap these updates into functions so that if a certain parameter is passed it will skip updating the system.
-	if [[ "$i{upgrade_kernel}" == "false" ]]; then
+	# TODO: Wrap these updates into functions so that if a certain
+	# parameter is passed it will skip updating the system.
+
+	if [[ "${upgrade_kernel}" == "false" ]]; then
 		return 0
 	fi
-	
 
 	# Update kernel from 3.2 > 3.8
-	apt-get-update && apt-get install -y --install-recommends linux-generic-lts-raring
+	apt-get-update
+	apt-get install -y --install-recommends linux-generic-lts-raring
 }
 
 update_os () {
@@ -236,12 +313,13 @@ update_os () {
 		return 0
 	fi
 
-	# Ensure necessary packages are installed and up to date, and install Dell OpenManage for Ubuntu
+	# Ensure necessary packages are installed and up to date
 	apt-get update && apt-get -y dist-upgrade
 }
 
 install_tools () {
-	apt-get update && apt-get install -y dsh curl ethtool ifenslave vim sysstat linux-crashdump
+	apt-get update && apt-get install -y dsh curl ethtool ifenslave vim \
+							sysstat linux-crashdump
 	sed -i 's/ENABLED=\"false\"/ENABLED=\"true\"/' /etc/default/sysstat
 }
 
@@ -250,7 +328,8 @@ install_dell_om () {
 		return 0
 	fi
 	
-	echo 'deb http://linux.dell.com/repo/community/deb/latest /' > /etc/apt/sources.list.d/linux.dell.com.list
+	echo 'deb http://linux.dell.com/repo/community/deb/latest /' \
+				> /etc/apt/sources.list.d/linux.dell.com.list
 	gpg --keyserver pool.sks-keyservers.net --recv-key 1285491434D8786F
 	gpg -a --export 1285491434D8786F | sudo apt-key add -
 	apt-get update && apt-get -y install srvadmin-all
@@ -260,20 +339,23 @@ install_dell_om () {
 	sleep 35
 
 	# Enable the performance profile in the BIOS
-	/opt/dell/srvadmin/bin/omconfig chassis biossetup attribute=SysProfile setting=PerfOptimized
+	"${OMCONFIG_BIN}" chassis biossetup attribute=SysProfile \
+							setting=PerfOptimized
 	sleep 10
 }
 
 restart_node () {
 	# Only restart if needed unless specifically told not to restart
-	if [[ "$i{restart_node}" == "false" ]]; then
+	if [[ "${restart_node}" == "false" ]]; then
 		return 0
 	fi
 
-	if [[ "$i{restart_needed}" == "true" ]]; then
+	if [[ "${restart_needed}" == "true" ]]; then
 		reboot
 	fi
 }
+
+parameters
 
 networking
 volumes
