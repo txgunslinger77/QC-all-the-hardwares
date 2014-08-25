f9a9b9293d57805c5c1b1e867e93e517cb21c92d
txgunslinger77 <tmaretzki@gmail.com>
2014-08-25 16:14:31 -0500

    fixed dell repo to 7.4

--- qc_script_1.sh
+++ qc_script_1.sh
@@ -362,7 +362,7 @@ install_dell_om () {
 		return 0
 	fi
 	
-	echo 'deb http://linux.dell.com/repo/community/deb/latest /' \
+	echo 'deb http://linux.dell.com/repo/community/ubuntu precise openmange/740' \
 				> /etc/apt/sources.list.d/linux.dell.com.list
 	gpg --keyserver pool.sks-keyservers.net --recv-key 1285491434D8786F
 	gpg -a --export 1285491434D8786F | sudo apt-key add -
